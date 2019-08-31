8f1cfe360e09eaa1212f67c22edc07422ba97150
enterkvas <enterkvas@gmail.com>
2019-08-31 17:58:14 +0300

    The eighth commit - the implementation of adding text from a text fild to a new paragraph

--- app.js
+++ app.js
@@ -1,7 +1,12 @@
 var main = function () {
     "use strict";
     $(".comment-input button").on("click", function (event) {
-        console.log("Hello, World!");
+        // var $new_comment = $("<p>");       
+        // var $new_comment = $("<p>").text("Это новый комментарий");
+        var $comment_text = $(".comment-input input").val();
+        // $new_comment.text($comment_text);        
+        var $new_comment = $("<p>").text($(".comment-input input").val()); 
+        $(".comments").append($new_comment);       
     });
 };
 $(document).ready(main);
