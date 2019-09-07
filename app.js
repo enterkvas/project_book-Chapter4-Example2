b1fbde02f89730943833115e69a656577988737c
enterkvas <enterkvas@gmail.com>
2019-09-07 19:28:09 +0300

    Twelfth commit - simplified app.js file code

--- app.js
+++ app.js
@@ -247,32 +247,32 @@
 // Но, для того, чтобы сработал метод fadeIn()? необх-мо, чтобы этот элемент ВНАЧАЛЕ был скрыт.
 // Для этого применяет перед методом fadeIn() метод hide():
 
-var main = function () {
-    "use strict";
-    $(".comment-input button").on("click", function (event) {
-        var $new_comment;
-        if ($(".comment-input input").val() !== "") {
-            $new_comment = $("<p></p>").text($(".comment-input input").val());
-            $new_comment.hide();
-            $(".comments").append($new_comment);
-            $new_comment.fadeIn();
-            $(".comment-input input").val("").focus();
-        }
-    })
-    $(".comment-input input").on("keypress", function (event) {
-        var $new_comment;
-        if (event.keyCode == 13) {
-            if ($(".comment-input input").val() !== "") {
-                $new_comment = $("<p></p>").text($(".comment-input input").val());
-                $new_comment.hide();
-                $(".comments").append($new_comment);
-                $new_comment.fadeIn();
-                $(".comment-input input").val("");
-            }
-        }                
-    })
-}
-$(document).ready(main);
+// var main = function () {
+//     "use strict";
+//     $(".comment-input button").on("click", function (event) {
+//         var $new_comment;
+//         if ($(".comment-input input").val() !== "") {
+//             $new_comment = $("<p></p>").text($(".comment-input input").val());
+//             $new_comment.hide();
+//             $(".comments").append($new_comment);
+//             $new_comment.fadeIn();
+//             $(".comment-input input").val("").focus();
+//         }
+//     })
+//     $(".comment-input input").on("keypress", function (event) {
+//         var $new_comment;
+//         if (event.keyCode == 13) {
+//             if ($(".comment-input input").val() !== "") {
+//                 $new_comment = $("<p></p>").text($(".comment-input input").val());
+//                 $new_comment.hide();
+//                 $(".comments").append($new_comment);
+//                 $new_comment.fadeIn();
+//                 $(".comment-input input").val("");
+//             }
+//         }                
+//     })
+// }
+// $(document).ready(main);
 
 // отсебятина: в 1-й части кода добавил метод focus(), т.к. в отличие от keypress в методе click после нажатия на кнопку + в поле текстовгог ввода теряется фокус.
 
@@ -301,9 +301,31 @@ $(document).ready(main);
 // }
 // $(document).ready(main);
 
+            // Переработка для упрощения
 
+// Для того, чтобы соблюсти Принцип разработки DRY, делаем дублирующийся код АБСТРАКТНОЙ ФУНКЦИЕЙ.
+// Эту ф-цию объявляем переменной для хранения ф-ций.
+// далее вызываем и определяем эту ф-цию в каждом из обработчиков событий. 
 
-
-
-
- 
+    var main = function () {
+        "use strict";        
+        var addCommentFromInputBox = function () {
+            var $new_comment;
+            if ($(".comment-input input").val() !== "") {
+                $new_comment = $("<p></p>").text($(".comment-input input").val());
+                $new_comment.hide();
+                $(".comments").append($new_comment);
+                $new_comment.fadeIn();
+                $(".comment-input input").val("").focus();
+            }
+        }
+        $(".comment-input button").on("click", function (event) {
+            addCommentFromInputBox();
+        })
+        $(".comment-input input").on("keypress", function (event) {            
+            if (event.keyCode == 13) {
+                addCommentFromInputBox();
+            }                
+        })
+    }
+    $(document).ready(main);
